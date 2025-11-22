a6e08df3624a7b1e45e8b9b95019995d7a012a6f
Flavio Massimi <massimiflavio95@gmail.com>
2025-11-22 11:53:37 +0100

    aggiunta script

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,6 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  base: '/studio-mua/',  // esatto nome del repo
+});

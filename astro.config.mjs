bd2d991c1f1c3503826be652f1c4ed7c5b41e7e5
omGazz <omgazz89@gmail.com>
2025-11-28 11:26:32 +0100

    fix: changed path relative and absolute of the site

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  base: '/studio-mua/',  // esatto nome del repo
+  base: '/studio-mua',  // esatto nome del repo
+  site: 'https://fmax95.github.io',
 });

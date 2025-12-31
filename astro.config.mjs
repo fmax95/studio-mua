4c22e26fd4687817c1d2103bb957d5e4305bca41
omGazz <omgazz89@gmail.com>
2025-12-31 20:50:10 +0100

    try fix 404

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  base: '/studio-mua',  // esatto nome del repo
-  site: 'https://fmax95.github.io',
+  site: 'https://studiomuadesign.it',
 });

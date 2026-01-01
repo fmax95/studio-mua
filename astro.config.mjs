073ba1e1660d162fd1c33f7162200cae66912fe9
omGazz <omgazz89@gmail.com>
2026-01-01 16:29:40 +0100

    added ita eng support

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,4 +4,11 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://studiomuadesign.it',
+  i18n: {
+    defaultLocale: "it",
+    locales: ["it", "en"],
+    routing: {
+      prefixDefaultLocale: false
+    }
+  }
 });

e31d8e3339513c5f3a3564259db447710abf27c8
omGazz <omgazz89@gmail.com>
2026-01-01 13:36:46 +0100

    fiix path with domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://fmax95.github.io',
-  base: '/studio-mua',
+  site: 'https://studiomuadesign.it',
 });

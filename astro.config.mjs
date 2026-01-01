f78912edc3e33d41713fa9e1d26b022bf46efeb1
omGazz <omgazz89@gmail.com>
2026-01-01 12:59:48 +0100

    fix path removed base

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://studiomuadesign.it',
+  site: 'https://fmax95.github.io',
+  base: '/studio-mua',
 });
